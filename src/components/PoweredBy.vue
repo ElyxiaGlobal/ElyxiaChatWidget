402aa86bb5ff7a23319eec76a5ee002c49e6e61c
ElyxiaGlobal <phil@elyxia.uk>
2025-07-15 21:47:14 +0800

    Initial commit: White-labeled n8n chat widget with Elyxia branding

---
+++ src/components/PoweredBy.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="chat-powered-by">
+		Powered by
+		<a href="https://elyxia.uk">Elyxia Global Limited</a>
+	</div>
+</template>
+
+<style lang="scss">
+.chat-powered-by {
+	text-align: center;
+
+	a {
+		color: var(--chat--color-primary);
+		text-decoration: none;
+	}
+}
+</style>
